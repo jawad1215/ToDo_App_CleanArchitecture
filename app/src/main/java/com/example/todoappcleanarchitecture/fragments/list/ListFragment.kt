4d7db75dff06dcfe3a2e9d5ce905c0ab59de4f14
jawad1215 <jawadyousafkhan@gmail.com>
2021-03-18 23:35:47 +0500

    List Fragment Menu added

--- app/src/main/java/com/example/todoappcleanarchitecture/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoappcleanarchitecture/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todoappcleanarchitecture.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.todoappcleanarchitecture.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -25,7 +23,12 @@ class ListFragment : Fragment() {
         view.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment2)
         }
+
+        setHasOptionsMenu(true)
         return view
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu,menu)
+    }
 }
